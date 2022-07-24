900f766e57f451612443d73ba7bcc5c6c7ac174b
lakshya agrawal <75083000+lakshya424agrawal@users.noreply.github.com>
2022-07-24 16:44:22 +0530

    Add files via upload

---
+++ Tictactoe.cpp
@@ -0,0 +1,105 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+char matrix[3][3] = {'1','2','3','4','5','6','7','8','9'};
+char player = 'X';
+
+
+void draw()
+{
+    system("cls"); // Window goes clean after every entry;
+    
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout<<matrix[i][j]<<" ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
+// 00 01 02
+// 10 11 12
+// 20 21 22
+char program_tic()
+{
+    // horizontal check;
+    if(matrix[0][0] == matrix[0][1] && matrix[0][0] == matrix[0][2])
+    {return matrix[0][0];}
+    else if(matrix[1][0] == matrix[1][1] && matrix[1][0] == matrix[1][2])
+    {return matrix[1][0];}
+    else if(matrix[2][0] == matrix[2][1] && matrix[2][0] == matrix[2][2])
+    {return matrix[2][0];}
+
+    //vertical check;
+    else if(matrix[0][0] == matrix[1][0] && matrix[0][0] == matrix[2][0])
+    {return matrix[0][0];}
+    else if(matrix[0][1] == matrix[1][1] && matrix[0][1] == matrix[2][1])
+    {return matrix[0][1];}
+    else if(matrix[0][2] == matrix[1][2] && matrix[0][2] == matrix[2][2])
+    {return matrix[0][2];}
+
+    //diagnal
+    else if(matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2])
+    {return matrix[0][0];}
+    else if(matrix[2][0] == matrix[1][1] && matrix[2][0] == matrix[0][2])
+    {return matrix[2][0];}
+
+    return '/';
+    
+}
+void tople()
+{
+    if(player == 'X')
+    player = 'O';
+    else 
+    player = 'X';
+}
+void Input()
+{
+    
+    int pos;
+    cin>>pos;
+    
+    if(pos == 1)
+    {matrix[0][0] = player;}
+    else if(pos == 2)
+    {matrix[0][1] = player;}
+    else if(pos == 3)
+    {matrix[0][2] = player;}
+    else if(pos == 4)
+    {matrix[1][0] = player;}
+    else if(pos == 5)
+    {matrix[1][1] = player;}
+    else if(pos == 6)
+    {matrix[1][2] = player;}
+    else if(pos == 7)
+    {matrix[2][0] = player;}
+    else if(pos == 8)
+    {matrix[2][1] = player;}
+    else if(pos == 9)
+    {matrix[2][2] = player;}
+
+    tople();
+
+}
+int main()
+{
+    draw();
+    while (1)
+    {
+        Input();
+        draw();
+        if(program_tic() == 'X')
+        {cout<<"Player X Win!!!";
+        break;}
+        else if(program_tic() == 'O')
+        {cout<<"Player O Win!!!";
+        break;}
+    }
+    
+
+}
