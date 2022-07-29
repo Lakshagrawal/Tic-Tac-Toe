eb77b73fa6e432141f879a63619d82a548787bac
lakshya agrawal <75083000+lakshya424agrawal@users.noreply.github.com>
2022-07-29 15:07:59 +0530

    Update Tictactoe.cpp

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 
-using namespace std;
 
+using namespace std;
+int vis[10]={0};
 char matrix[3][3] = {'1','2','3','4','5','6','7','8','9'};
 char player = 'X';
 
@@ -47,7 +48,7 @@ char program_tic()
     {return matrix[0][0];}
     else if(matrix[2][0] == matrix[1][1] && matrix[2][0] == matrix[0][2])
     {return matrix[2][0];}
-
+    
     return '/';
     
 }
@@ -63,7 +64,12 @@ void Input()
     
     int pos;
     cin>>pos;
-    
+    if(vis[pos] == 1)
+    {
+    	cout << "Repeated Input\n";
+    }
+    else{
+    vis[pos]=1;
     if(pos == 1)
     {matrix[0][0] = player;}
     else if(pos == 2)
@@ -83,23 +89,55 @@ void Input()
     else if(pos == 9)
     {matrix[2][2] = player;}
 
-    tople();
+    tople();}
 
 }
 int main()
 {
-    draw();
-    while (1)
-    {
-        Input();
+    system("cls");
+    play_again:
+
+    cout<<"Wellcome to the Tic-Tac_Toe game"<<endl;
+    cout<<"Press key according to play"<<endl;
+    cout<<"1) Press 1 for Double player"<<endl;
+    cout<<"2) Press 2 for Exit the game"<<endl;
+    int a ;
+    cin>>a;
+    if(a == 1)
+   { 
         draw();
-        if(program_tic() == 'X')
-        {cout<<"Player X Win!!!";
-        break;}
-        else if(program_tic() == 'O')
-        {cout<<"Player O Win!!!";
-        break;}
+
+        while (1)
+        {
+            Input();
+            draw();
+            if(program_tic() == 'X')
+            {cout<<"Player X Win!!!";
+            break;}
+            else if(program_tic() == 'O')
+            {cout<<"Player O Win!!!";
+            break;}
+            else
+            {
+                bool ok = true;
+                for(int i=1;i<=9;i++)
+                if(!vis[i])
+                ok = false;
+
+                if(ok)
+                {cout << "Tie Nobody Win";
+                break;
+                cout<<endl;
+                cout<<"If you wand to try again press 1";
+                int a;
+                cin>>a;
+                if(a == 1)
+                {goto play_again;}}
+            }
+        }
     }
+    else
+    {system("exit");}
     
 
 }
